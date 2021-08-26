ddd4c36f80b20932cb07ac9fd3d0114a76d624ae
gisaklc <gisaklc@hotmail.com>
2021-08-26 16:09:41 -0300

    criando consulta com query method

--- src/main/java/com/gisaklc/workshopmongo/resource/PostResource.java
+++ src/main/java/com/gisaklc/workshopmongo/resource/PostResource.java
@@ -7,9 +7,11 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.gisaklc.workshopmongo.domain.Post;
+import com.gisaklc.workshopmongo.resource.utils.URL;
 import com.gisaklc.workshopmongo.service.PostService;
 
 @RestController
@@ -31,4 +33,12 @@ public class PostResource {
 		return ResponseEntity.ok().body(list);
 	}
 
+	@RequestMapping(value="/titlesearch", method=RequestMethod.GET)
+	public ResponseEntity<List<Post>> findByTitle(@RequestParam(value = "text", defaultValue="") String text) {
+		text = URL.decodeParam(text);
+		List<Post> list = postService.findByTitle(text);
+		return ResponseEntity.ok().body(list);
+	}
+
+	
 }
