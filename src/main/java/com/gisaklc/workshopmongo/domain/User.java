88806eaae8b83d2878bcc7b9cfb74b3867a8835c
gisaklc <gisaklc@hotmail.com>
2021-08-26 12:13:35 -0300

    adicionando a referencia de Posts na classe usuario

--- src/main/java/com/gisaklc/workshopmongo/domain/User.java
+++ src/main/java/com/gisaklc/workshopmongo/domain/User.java
@@ -1,8 +1,11 @@
 package com.gisaklc.workshopmongo.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document //mapeamento da classe mongodb
@@ -16,7 +19,11 @@ public class User implements Serializable {
 	private String id;
 	private String nome;
 	private String email;
-
+	//dbref é para referenciar a collection 
+	@DBRef(lazy=true)//o laze true carregar os posts somente se acessar explicitamente, ou seja
+	private List<Post> posts = new ArrayList<>();
+	
+	
 	public User() {
 		super();
 		// TODO Auto-generated constructor stub
@@ -56,6 +63,15 @@ public class User implements Serializable {
 
 	public void setEmail(String email) {
 		this.email = email;
+	}
+	
+	
+	public List<Post> getPosts() {
+		return posts;
+	}
+
+	public void setPosts(List<Post> posts) {
+		this.posts = posts;
 	}
 
 	@Override
