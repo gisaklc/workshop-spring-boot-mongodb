98eb7b7274a5a7df5a82747fadd91b3f819d8a11
gisaklc <gisaklc@hotmail.com>
2021-08-26 12:43:57 -0300

    endpoint Post por id

--- src/main/java/com/gisaklc/workshopmongo/service/PostService.java
+++ src/main/java/com/gisaklc/workshopmongo/service/PostService.java
@@ -1,5 +1,25 @@
 package com.gisaklc.workshopmongo.service;
 
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.gisaklc.workshopmongo.domain.Post;
+import com.gisaklc.workshopmongo.repository.PostRepository;
+import com.gisaklc.workshopmongo.service.exception.ObjectNotFoundException;
+
+@Service
 public class PostService {
 
+	@Autowired
+	private PostRepository postRepository;
+	
+	public Post findById(String id) {
+		Optional<Post> obj = postRepository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+
+	}
+	
+	
 }
